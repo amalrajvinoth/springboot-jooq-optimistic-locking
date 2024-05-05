6a8b26976e78f61b42c5e3aa920486084efbc4a5
Amal <amalrajvinoth@gmail.com>
2024-05-05 12:31:56 +0200

    Initial version

---
+++ src/test/java/io/github/amalrajvinoth/optimisticlocking/OptimisticLockingApplicationIT.java
@@ -0,0 +1,14 @@
+package io.github.amalrajvinoth.optimisticlocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimisticLockingApplicationIT {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
